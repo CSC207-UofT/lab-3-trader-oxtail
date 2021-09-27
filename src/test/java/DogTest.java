69ad57a901fcb5eb94f96bd96f68ae0359e8cb67
canyildiz02 <canyildiz02@gmail.com>
2021-09-27 14:04:21 -0400

    I hope I'm doing it right

---
+++ src/test/java/DogTest.java
@@ -0,0 +1,13 @@
+/*
+ * This file contains sample JUnit test cases for Horse.java
+ * You should NOT modify this file!
+ */
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+
+
+public class DogTest {
+}
